16f0386aade22c975d2803dcd664549b9b63e50a
nekobato <nekobato@gmail.com>
2017-03-16 18:14:35 +0900

    install prezto

--- install.sh
+++ install.sh
@@ -1,16 +1,42 @@
-#/bin/bash
+#
+# Setup Zsh enviroments
+#
+if [ ! -e ~/.zprezto ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
 
 if [ ! -e ~/.zshrc ]; then
-  ln -s ~/dotfiles/zshrc ~/.zshrc
+  ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 fi
 
 if [ ! -e ~/.zshrc.local ]; then
   touch ~/.zshrc.local
 fi
 
-if [ ! -e ~/.zshenv ]; then
-  ln -s ~/dotfiles/zshenv ~/.zshenv
+if [ ! -e ~/.zpreztorc ]; then
+  ln -s ~/dotfiles/zsh/zpreztorc ~/.zpreztorc
 fi
+
+if [ ! -e ~/.zshenv ]; then
+  ln -s ~/dotfiles/zsh/zshenv ~/.zshenv
+fi
+
+if [ ! -e ~/.zshenv.local ]; then
+  touch ~/.zshenv.local
+fi
+
+if [ ! -e ~/.zprofile ]; then
+  ln -s ~/dotfiles/zsh/zprofile ~/.zprofile
+fi
+
+if [ ! -e "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions/prompt_nekobato_setup" ]; then
+  ln -s ~/dotfiles/zsh/prompt_nekobato_setup "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions/prompt_nekobato_setup"
+fi
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
 if [ ! -e ~/.tmux.conf ]; then
   ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
